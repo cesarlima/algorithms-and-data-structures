2d13fdf791c6af8026e87994784b2ee5d283c5d1
Cesar Lima <cesarcamposdelima@gmail.com>
2024-08-07 06:07:58 -0300

    `SinglyLinkedList.push` sets element as head and tail and increments count on empty list.

--- AlgorithmsAndDataStructure/AlgorithmsAndDataStructureTests/DataStructure/SinglyLinkedListTests.swift
+++ AlgorithmsAndDataStructure/AlgorithmsAndDataStructureTests/DataStructure/SinglyLinkedListTests.swift
@@ -7,13 +7,24 @@
 
 import XCTest
 
-final class Node {
+final class Node<T: Equatable>: Equatable {
+    private(set) var value: T
+    private(set) var next: Node<T>?
     
+    init(value: T, next: Node<T>? = nil) {
+        self.value = value
+        self.next = next
+    }
+    
+    static func == (lhs: Node<T>, rhs: Node<T>) -> Bool {
+        return lhs.value == rhs.value
+        && lhs.next?.value == rhs.next?.value
+    }
 }
 
-final class SinglyLinkedList {
-    private(set) var head: Node?
-    private(set) var tail: Node?
+final class SinglyLinkedList<T: Equatable> {
+    private(set) var head: Node<T>?
+    private(set) var tail: Node<T>?
     private(set) var count: Int
     
     init() {
@@ -21,16 +32,32 @@ final class SinglyLinkedList {
         self.tail = nil
         self.count = 0
     }
+    
+    func push(_ element: Node<T>) {
+        head = element
+        tail = element
+        count += 1
+    }
 }
 
 final class SinglyLinkedListTests: XCTestCase {
         
     func test_init_allPropertiesAreEmpty() {
-        let sut = SinglyLinkedList()
+        let sut = SinglyLinkedList<Int>()
         
         XCTAssertNil(sut.head)
         XCTAssertNil(sut.tail)
         XCTAssertEqual(sut.count, 0)
     }
-
+    
+    func test_push_setsHeadTailAndIncrementsCountOnEmptyList() {
+        let sut = SinglyLinkedList<Int>()
+        let node = Node(value: 1)
+        
+        sut.push(node)
+        
+        XCTAssertEqual(sut.head, node)
+        XCTAssertEqual(sut.tail, node)
+        XCTAssertEqual(sut.count, 1)
+    }
 }
