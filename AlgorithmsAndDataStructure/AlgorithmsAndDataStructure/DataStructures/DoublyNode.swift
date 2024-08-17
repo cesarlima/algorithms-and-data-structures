ac7cef8aa1cdf67f3b09685dd9b6d0957bfdd9f0
Cesar Lima <cesarcamposdelima@gmail.com>
2024-08-17 18:31:20 -0300

    Add `DoublyNode`

---
+++ AlgorithmsAndDataStructure/AlgorithmsAndDataStructure/DataStructures/DoublyNode.swift
@@ -0,0 +1,26 @@
+//
+//  DoublyNode.swift
+//  AlgorithmsAndDataStructure
+//
+//  Created by MacPro on 17/08/24.
+//
+
+import Foundation
+
+final class DoublyNode<T: Equatable> {
+    private(set) var value: T
+    private(set) var next: DoublyNode<T>?
+    private(set) var prev: DoublyNode<T>?
+    
+    init(value: T) {
+        self.value = value
+    }
+    
+    func setNext(_ next: DoublyNode<T>?) {
+        self.next = next
+    }
+    
+    func setPrevious(_ prev: DoublyNode<T>?) {
+        self.prev = prev
+    }
+}
